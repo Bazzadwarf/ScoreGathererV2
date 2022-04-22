0a789eac30ca15b9c426682926bc08c742efe308
Louis Hobbs <bazzadobs@live.co.uk>
2022-04-22 17:38:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ScoreGathererV2
+{
+    class Program
+    {
+        static string clientId = "";
+        static string clientSecret = "";
+
+        static string apiUrl = "https://osu.ppy.sh/api/v2";
+        static string tokenUrl = "https://osu.ppy.sh/oauth/token";
+        static string accessToken = null;
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Loading...");
+
+            accessToken = await GetToken();
+            Console.WriteLine(accessToken != null ? "Got Token!!!" : "No token found!" );
+            
+
+            bool dateNotFalse = false;
+            List<Score> Scores = new List<Score>();
+            string mode = "taiko";
+            ulong scoreID = 1204396;
+            DateTime lastTime = new DateTime(2007, 1, 1);
+
+            while (lastTime < new DateTime(2009, 9, 13))
+            {
+                Console.WriteLine("Currently Checking: " + scoreID + "\tScores found: " + Scores.Count + "\tCurrent Time: " + lastTime);
+                var score = await GetScore(mode, scoreID);
+
+                if (score != null)
+                {
+                    string dateString = ((dynamic)score).created_at;
+                    lastTime = DateTime.Parse(dateString, CultureInfo.InvariantCulture);
+                    if (DateTime.Parse(dateString, CultureInfo.InvariantCulture) >= new DateTime(2009,9,13))
+                    {
+                        continue;
+                    }
+
+                    if (((dynamic)score).beatmap.status != "ranked")
+                    {
+                        scoreID++;
+                        continue; 
+                    }
+                        
+
+                    var newScore = new Score();
+
+                    newScore.Beatmapset = ((dynamic)score).beatmap.beatmapset_id;
+                    newScore.Beatmap = ((dynamic)score).beatmap.id;
+                    newScore.ScoreID = scoreID;
+                    newScore.RankedScore = ((dynamic)score).score;
+                    newScore.Username = ((dynamic)score).user.username;
+                    newScore.Count300 = ((dynamic)score).statistics.count_300;
+                    newScore.Count100 = ((dynamic)score).statistics.count_100;
+                    newScore.Count50 = ((dynamic)score).statistics.count_50;
+                    newScore.Countmiss = ((dynamic)score).statistics.count_miss;
+                    newScore.Maxcombo = ((dynamic)score).max_combo;
+                    newScore.Countgeki = ((dynamic)score).statistics.count_geki;
+                    newScore.Countkatu = ((dynamic)score).statistics.count_katu;
+                    newScore.Perfect = ((dynamic)score).perfect;
+                    newScore.UserID = ((dynamic)score).user.id;
+                    newScore.Date = DateTime.Parse(dateString, CultureInfo.InvariantCulture);
+                    newScore.Rank = ((dynamic)score).rank;
+                    newScore.ReplayAvailable = ((dynamic)score).replay;
+
+                    Scores.Add(newScore);
+                }
+                scoreID++;
+            }
+
+            TextWriter textWriter = new StreamWriter("7thday.csv");
+            var ucsv = new CsvWriter(textWriter, CultureInfo.InvariantCulture);
+
+            ucsv.WriteRecords(Scores);
+            ucsv.Flush();
+            textWriter.Close();
+
+        }
+
+        private static async Task<string> GetToken()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(tokenUrl);
+
+                // Set the intended response to be JSON.
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+                // Build the data to POST.
+                List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
+                postData.Add(new KeyValuePair<string, string>("client_id", clientId));
+                postData.Add(new KeyValuePair<string, string>("client_secret", clientSecret));
+                postData.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+                postData.Add(new KeyValuePair<string, string>("scope", "public"));
+
+                FormUrlEncodedContent content = new FormUrlEncodedContent(postData);
+
+                // Post the server and wait for the resonse
+                HttpResponseMessage responseMessage = await client.PostAsync("", content);
+                string jsonString = await responseMessage.Content.ReadAsStringAsync();
+                object responseData = JsonConvert.DeserializeObject(jsonString);
+
+                return ((dynamic)responseData).access_token;
+            }
+        }
+
+        private static async Task<object> GetScore(string mode, ulong ScoreID)
+        {
+            object responseData;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://osu.ppy.sh/api/v2/scores/" + mode + "/" + ScoreID.ToString());
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + accessToken);
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+                HttpResponseMessage responseMessage = await client.GetAsync(client.BaseAddress);
+
+                if (responseMessage.StatusCode != System.Net.HttpStatusCode.OK)
+                    return null;
+
+                string jsonString = await responseMessage.Content.ReadAsStringAsync();
+                responseData = JsonConvert.DeserializeObject(jsonString);
+            }
+            return responseData;
+        }
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreGathererV2
+{
+    class Score
+    {
+        private int beatmapset;
+        private int beatmap;
+        private ulong score_id;
+        private ulong score;
+        private string username;
+        private ulong count300;
+        private ulong count100;
+        private ulong count50;
+        private ulong countmiss;
+        private int maxcombo;
+        private int countkatu;
+        private int countgeki;
+        private bool perfect;
+        private int user_id;
+        private DateTime date;
+        private string rank;
+        private float pp;
+        private bool replay_available;
+
+        public int Beatmapset { get => beatmapset; set => beatmapset = value; }
+        public int Beatmap { get => beatmap; set => beatmap = value; }
+        public ulong ScoreID { get => score_id; set => score_id = value; }
+        public ulong RankedScore { get => score; set => score = value; }
+        public string Username { get => username; set => username = value; }
+        public ulong Count300 { get => count300; set => count300 = value; }
+        public ulong Count100 { get => count100; set => count100 = value; }
+        public ulong Count50 { get => count50; set => count50 = value; }
+        public ulong Countmiss { get => countmiss; set => countmiss = value; }
+        public int Maxcombo { get => maxcombo; set => maxcombo = value; }
+        public int Countkatu { get => countkatu; set => countkatu = value; }
+        public int Countgeki { get => countgeki; set => countgeki = value; }
+        public bool Perfect { get => perfect; set => perfect = value; }
+        public int UserID { get => user_id; set => user_id = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Rank { get => rank; set => rank = value; }
+        public float PP { get => pp; set => pp = value; }
+        public bool ReplayAvailable { get => replay_available; set => replay_available = value; }
+    }
+}
